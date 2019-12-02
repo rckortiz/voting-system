5be5e992209e26bcb7670e999e5d861643478e2d
Rick Ortiz <rick@Ricks-MacBook-Pro.local>
2019-12-02 10:56:23 -0500

    made some updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import './index.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      languages: [
+        { name: 'Php', votes: 0 },
+        { name: 'Python', votes: 0 },
+        { name: 'Go', votes: 0 },
+        { name: 'Java', votes: 0 }
+      ]
+    }
+  }
+
+  vote(i) {
+    let newLanguages = [...this.state.languages]
+    newLanguages[i].votes++
+    const swap = (array, i, j) => {
+      const temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    this.setState({ languages: newLanguages })
+  }
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <h1> Vote your Language!</h1>
+        <div className="languages">
+          {this.state.languages.map((lang, i) => (
+            <div key={i} className="language">
+              <div className="voteCount">{lang.votes}</div>
+              <div className="languageName">{lang.name}</div>
+              <button onClick={this.vote.bind(this, i)}>Click Here</button>
+            </div>
+          ))}
+        </div>
+      </main>
+    )
   }
 }
 
